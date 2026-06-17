5b3df96df097a5923865078ea2788e9731b02e4b
Émile <emile.hogue.03@gmail.com>
2026-06-17 19:34:25 -0400

    seeder script

---
+++ src/data/seed.js
@@ -0,0 +1,39 @@
+const { exit } = require("process");
+const Device = require("../models/device");
+const Measurement = require("../models/measurement");
+const fs = require('fs');
+const { connectDB } = require("../../src/services/mongoose");
+connectDB().catch(err => console.log(err));
+
+
+async function seed() {
+    // Seed des devices
+    const devices = JSON.parse(fs.readFileSync(__dirname + "/device_seed.json"));
+    for (element of devices) {
+        const exists = await Device.exists({ username: element.username });
+        if (!exists) { // assure que l'utilisateur n'existe pas
+            const device = new Device(element);
+            try {
+                await device.generateAuthTokenAndSaveDevice();
+            } catch (e) {
+                console.error(e);
+            }
+            console.log("Ajouté compte: ", device);
+        }
+    }
+
+    // Seed des mesures
+    const measurements = JSON.parse(fs.readFileSync(__dirname + "/measurement_seed.json"));
+    for (element of measurements) {
+        const exists = await Measurement.exists({ noise_buffer: element.noise_buffer, sound_buffer: element.sound_buffer, location: element.location });
+        if (!exists) { // assure que la mesure n'existe pas déjà
+            const measurement = new Measurement(element);
+            await measurement.save();
+            console.log("Mesure ajoutée");
+        }
+    }
+    console.log("Si aucun message d'ajout existe, les données ont déjà été seedé dans la base de données");
+    exit();
+}
+
+seed();
