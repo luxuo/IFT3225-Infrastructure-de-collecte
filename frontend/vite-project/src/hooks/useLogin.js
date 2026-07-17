5a6a8e8bafa61c89f64933646ae46fc73b755e09
Émile <emile.hogue.03@gmail.com>
2026-07-17 12:46:18 -0400

    logique de formulaire login

--- frontend/vite-project/src/hooks/useLogin.js
+++ frontend/vite-project/src/hooks/useLogin.js
@@ -1,22 +1,26 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useContext } from "react";
 import { fetchToken } from "../api/login.js";
+import { UserContext } from "../context/user.jsx";
 
-export default function useLogin(req) {
-  const [login, setLogin] = useState([]);
+export default function useLogin() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const {user, setUser} = useContext(UserContext);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
-  useEffect(() => {
+  const handleClick = (event) => {
+    event.preventDefault();
     setLoading(true);
 
-    fetchToken(req)
+    fetchToken({username, password})
       .then((data) => {
-        setLogin(data);
+        setUser(data);
         setError(null);
       })
       .catch((err) => setError(err.message))
       .finally(() => setLoading(false));
-  }, []);
+  };
 
-  return { login, loading, error };
+  return { setUsername, setPassword, handleClick, loading, error };
 }
--- frontend/vite-project/src/pages/Login.jsx
+++ frontend/vite-project/src/pages/Login.jsx
@@ -1,17 +1,22 @@
 import React, { useState, useEffect } from 'react';
 import AccountForm from '../components/AccountForm'
 import useLogin from '../hooks/useLogin';
+import LoadError from '../components/loading/LoadError';
+import Loading from '../components/loading/Loading';
 
 export default function(){
-    //const { login, loading, error } = useLogin();
+    const { setUsername, setPassword, handleClick, loading, error } = useLogin();
 
-    const handleClick = async (event) => {
-        event.preventDefault();
-    };
+    if (loading){
+        return (
+            <Loading loadMessage={"Connexion en cours..."} />
+        );
+    }
 
     return (
         <div>
-            <AccountForm statement={'Se Connecter'} handleClick={handleClick}/>
+            <AccountForm statement={'Se Connecter'} handleClick={handleClick} setUsername={setUsername} setPassword={setPassword}/>
+            {error? <LoadError errorMessage={error}/>:<></>}
         </div>
-    )
+    );
 }
