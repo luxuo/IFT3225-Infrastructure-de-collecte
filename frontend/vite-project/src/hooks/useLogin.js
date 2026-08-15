ff25482e48ccbc75fe0e3e571c0f2b806ac05b30
Émile <emile.hogue.03@gmail.com>
2026-08-15 19:22:05 -0400

    login signup validation

--- frontend/vite-project/src/hooks/useLogin.js
+++ frontend/vite-project/src/hooks/useLogin.js
@@ -1,6 +1,7 @@
 import { useEffect, useState, useContext } from "react";
 import { fetchToken } from "../api/login.js";
 import { UserContext } from "../context/user.jsx";
+import { searchUser } from "../api/searchUser.js";
 
 export default function useLogin(signup) {
   const [username, setUsername] = useState('');
@@ -11,15 +12,37 @@ export default function useLogin(signup) {
 
   const handleClick = (event) => {
     event.preventDefault();
+    const location = 'diddy';
+    setError(null);
+    // Empty strings
+    if(username==""){
+      setError("Nom d'utilisateur est obligatoire");
+      return;
+    }else if(password == ""){
+      setError("Mot de passe est obligatoire");
+      return;
+    }
+
     setLoading(true);
-    const location = 'diddy'
+    // username check
+    if (signup){
+      searchUser({username})
+      .then((data) => {if(data._id){setError("Utilisateur existe déjà")}})
+      .catch((err) => { setError(err.message)})
+    }
+
+    if (error != null){
+      setLoading(false);
+      return;
+    }
+
     const req = signup? {username, password, location}: {username, password}
     fetchToken(req)
       .then((data) => {
         setUser(data);
         setError(null);
       })
-      .catch((err) => setError(err.message))
+      .catch((err) => setError("Nom d'utilisateur ou mot de passe incorrecte"))
       .finally(() => setLoading(false));
   };
 
